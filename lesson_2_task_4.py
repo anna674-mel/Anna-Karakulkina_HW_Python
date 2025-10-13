17dba51e85ededa2c723343f1bb0b867b3ef37df
Anna Karakulkina <anna-pletenyova@yandex.ru>
2025-10-13 22:31:06 +0500

    Добавила пустую строку в конце файла

--- lesson_2_task_4.py
+++ lesson_2_task_4.py
@@ -3,7 +3,7 @@ n = int(input("Введите число:"))
 
 def fizz_buzz(n):
     for i in range(1, n + 1):
-        if (i % 3 == 0) and (i % 5 == 0):  
+        if (i % 3 == 0) and (i % 5 == 0):
             print(f"{i} - FizzBuzz")
         elif i % 3 == 0:
             print(f"{i} - Fizz")
